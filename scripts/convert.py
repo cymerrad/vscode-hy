963a5fa0b286f981a1698447d2a5e53b326602c6
Radosław Cymer <cymerrad@gmail.com>
2020-05-06 00:04:02 +0200

    yaml conversion

---
+++ scripts/convert.py
@@ -0,0 +1,54 @@
+#!python
+import json
+import plistlib
+import yaml
+import sys
+import os
+
+
+def convert(lib_in, file_in, lib_out, file_out):
+    tmp = lib_in.load(file_in)
+    lib_out.dump(tmp, file_out)
+
+
+class yaml_wrap:
+    @staticmethod
+    def load(fp):
+        return yaml.load(fp, Loader=yaml.FullLoader)
+
+    @staticmethod
+    def dump(obj, fp=None):
+        # TODO(rado) dumping with some strings as literal or folded: https://yaml-multiline.info/
+        # dumper = yaml.Dumper()
+
+        return yaml.dump(obj, fp)
+
+
+libs = {
+    ".json": json,
+    ".yaml": yaml_wrap,
+    ".tmLanguage": plistlib,
+}
+
+
+def main():
+    _, filename_in, filename_out, *_ = sys.argv
+    assert os.path.exists(filename_in)
+
+    lib_in, lib_out = None, None
+    for ending in libs.keys():
+        if filename_in.endswith(ending):
+            lib_in = libs[ending]
+
+        if filename_out.endswith(ending):
+            lib_out = libs[ending]
+
+    if lib_in is None or lib_out is None:
+        raise Exception("Unsupported conversion pair")
+
+    with open(filename_in, "rb") as fr, open(filename_out, "w") as fw:
+        convert(lib_in, fr, lib_out, fw)
+
+
+if __name__ == "__main__":
+    main()
