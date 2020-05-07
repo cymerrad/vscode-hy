f350b2bab187485fd84891cb2d97c170adbeadfa
cymerrad <cymerrad@gmail.com>
2020-05-07 19:41:53 +0200

    Clojure rip-off (#1)
    * Minor changes to original
    
    * prebuilt
    
    * No longer commiting 'built' syntax
    
    * Changing some colors
    
    * update
    
    * Fun fact: mismatched parens are almost impossible to debug
    
    * Failsafe for incorrect builds
    
    * Removed for good
    
    * Save point
    
    * HOLY MOTHER O MATKO IT WORKS
    
    * Regexp validation
    
    * Check for empty tokens
    
    * Copy file infix
    
    * More ideas
    
    * Hiding the evidence
    
    * More work
    
    * Description
    
    * More work, less ranting
    
    * Control statements
    
    * Cute af

--- README.md
+++ README.md
@@ -1,18 +1,27 @@
 # Hylang support for VSCode
-Based on `sjhuangx/vscode-scheme` and `xuqinghan/vscode-hy`, but is much more than just a REGEXP replace of `scheme -> hy`.
-Also a bit modernized and fixed where possible (WIP).
-
-# Done
-- using .json instead of .xml
-- fixed single-quote pairing
-- removed scheme-only keywords
+Syntax file *borrowed* from VSCode's Clojure built-in, but converted into YAML for readability<sup>[citation needed]</sup>.
+Colors for tokens are as similar to Python's as possible.
 
 # TODO
-- json from more readable [citation needed] yaml
-- illegal parenthesis
-- lists and dictionaries
-- remove non-built-in symbols
-- octals and hexes
+- **convert YAML on npm install**
+- sieve through built-in and non-built-in symbols
+- consistent hy/hylang naming around the project
+- separate rules for classes, lambdas, macros and imports
 - format & bracket string (https://docs.hylang.org/en/stable/language/syntax.html#string-literals)
-- keywords
-- discard prefix
+- function variadic args & metadata
+- some special symbols like #_, #* etc.
+- for, lfor, gfor syntax help
+- special highlight for regexes
+- built-in exceptions highlight
+- **maybe at that point publish?**
+- screenshots
+
+# Done
+- using JSON instead of XML
+- using YAML instead of JSON
+- sensible colors & paren matching
+- fixed single-quote pairing
+- lists, sets and dictionaries
+- function definitions
+- fixed paren matching
+- octals and hexes
--- scripts/convert.py
+++ scripts/convert.py
@@ -4,11 +4,19 @@ import plistlib
 import yaml
 import sys
 import os
+import shutil
+import re
 
 
-def convert(lib_in, file_in, lib_out, file_out):
-    tmp = lib_in.load(file_in)
-    lib_out.dump(tmp, file_out)
+def traverse_dict(dd, key, cb):
+    if type(dd) == dict:
+        for k, v in dd.items():
+            traverse_dict(v, f"{key}.{k}", cb)
+    elif type(dd) == list:
+        for ind, el in enumerate(dd):
+            traverse_dict(el, f"{key}.{ind}", cb)
+    else:
+        cb(key, dd)
 
 
 class yaml_wrap:
@@ -24,16 +32,72 @@ class yaml_wrap:
         return yaml.dump(obj, fp)
 
 
+class json_wrap:
+    @staticmethod
+    def load(*args, **kwargs):
+        return json.load(*args, **kwargs)
+
+    @staticmethod
+    def dump(obj, *args, **kwargs):
+        (fails, _) = json_wrap.check_regexes_correctness(obj)
+
+        if len(fails):
+            err_msg = f"Found {len(fails)} error{'s' if len(fails) > 1 else ''}"
+            footer = "-" * len(err_msg)
+            print(footer)
+            print(err_msg)
+
+            for where, fail in fails.items():
+                print(f"In: {where}\n\t{fail}")
+
+            print(footer)
+
+        return json.dump(obj, *args, **kwargs)
+
+    @staticmethod
+    def check_regexes_correctness(dictionary):
+        fails = {}
+        warnings = {}
+
+        def check_re(key, pattern):
+            nonlocal fails
+            for ending in ["begin", "match", "end"]:
+                if key.endswith(ending):
+                    break
+            else:
+                return
+
+            try:
+                rex = re.compile(pattern)
+            except re.error as e:
+                fails[key] = e
+
+            if rex.match("") and key.endswith("match"):
+                fails[key] = "Matches empty string"
+
+        traverse_dict(dictionary, "{}", check_re)
+        return fails, warnings
+
+
 libs = {
-    ".json": json,
+    ".json": json_wrap,
     ".yaml": yaml_wrap,
     ".tmLanguage": plistlib,
 }
 
 
+def convert(lib_in, file_in, lib_out, file_out):
+    tmp = lib_in.load(file_in)
+    lib_out.dump(tmp, file_out)
+
+
 def main():
     _, filename_in, filename_out, *_ = sys.argv
     assert os.path.exists(filename_in)
+    if os.path.exists(filename_out):
+        parts = filename_out.split(".")
+        filename_copy = ".".join(parts[:-1] + ["_copy"] + parts[-1:])
+        shutil.move(filename_out, filename_copy)
 
     lib_in, lib_out = None, None
     for ending in libs.keys():
